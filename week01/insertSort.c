9c07719bff7faadc337612e5f080f628ca1aced3
smilejun <dreamwe@nhnnext.org>
2015-03-05 15:56:05 +0900

    insertSort implement start

---
+++ week01/insertSort.c
@@ -0,0 +1,34 @@
+/*
+ * insertSort.c
+ * Insertion Sort 함수 구현
+ * 
+ * Created by Son Myeong Jun on 2015. 03. 05..
+ * Copyright 2015. Son Myeong Jun all rights reserved.
+ */
+
+#include <stdio.h>
+#include <assert.h>
+
+#define TRUE 1
+#define FALSE 0
+
+void insertionSort(int *arr, int length); 
+int isSorted(int *arr, int num);
+int assertTestCase();
+
+int main(void)
+{
+    int * arr = NULL;
+    return 0;
+}
+
+void insertionSort(int *arr, int length)
+{
+
+}
+
+int isSorted(int *arr, int num)
+{
+    return TRUE;
+}
+
