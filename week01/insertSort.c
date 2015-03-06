48d91c8768c50d65494cb4b89bb21c212163a3de
smilejun <dreamwe@nhnnext.org>
2015-03-06 10:58:02 +0900

    #1 class study applied

--- week01/insertSort.c
+++ week01/insertSort.c
@@ -13,22 +13,50 @@
 #define FALSE 0
 
 void insertionSort(int *arr, int length); 
-int isSorted(int *arr, int num);
+int isSorted(int *arr, int length);
 int assertTestCase();
 
 int main(void)
 {
-    int * arr = NULL;
+    int arr[10] = {10,6,3,4,6,8,9,5,3,5};
+
+    insertionSort(arr, 10);
+    if(isSorted(arr, 10))
+    {
+        printf("is sorted\n");
+    }
     return 0;
 }
 
 void insertionSort(int *arr, int length)
 {
-
+    int lastIndex = length - 1;
+    int startIndex = 1;
+    
+    int index;
+    for (index = startIndex; index <= lastIndex; index++)
+    {
+        int target = arr[index];
+        int subIndex = index - 1;
+        while(target < arr[subIndex] && subIndex >= 0)
+        {
+            arr[subIndex + 1] = arr[subIndex];
+            subIndex--;
+        }
+        arr[subIndex + 1] = target;
+    }
 }
 
-int isSorted(int *arr, int num)
+int isSorted(int *arr, int length)
 {
+    int i;
+    for (i = 0; i < length - 1; i++)
+    {
+        if (arr[i] <= arr[i + 1])
+            continue;
+        else 
+            return FALSE;
+    }
     return TRUE;
 }
 
